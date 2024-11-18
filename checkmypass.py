4c21211287157de6a84f682975540ec87c93407b
KissAkos <kopyherceg@gmail.com>
2024-11-18 18:56:54 +0200

    finished password checker

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -12,7 +13,9 @@ def request_api_data(query_char):
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
@@ -20,4 +23,15 @@ def pwned_api_check(password):
     response =  request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times.')
+        else:
+            print(f'{password} was NOT found')
+    return 'Done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
